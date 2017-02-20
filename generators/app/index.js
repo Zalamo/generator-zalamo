308ec68334160f2dd5bd55776cf8d2b2d8b2d5d1
Daniel Busłowicz <draccoz@gmail.com>
2017-02-20 15:20:59 +0100

    Added a bit of readme

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,9 +5,11 @@ const yosay = require('yosay');
 
 module.exports = class extends Generator {
   prompting() {
-    this.log(yosay(`
-Yo! Please use one of the following generators:
-* zalamo:${chalk.green('module')}
-    `));
+    this.log(`Yo! Please use one of the following generators:
+* ${chalk.yellow('zalamo')}:${chalk.green('module')} ${chalk.blue('ModuleName')}
+* ${chalk.yellow('zalamo')}:${chalk.green('view')} ${chalk.blue('ModuleName')} ${chalk.cyan('ViewName')}
+* ${chalk.yellow('zalamo')}:${chalk.green('component')} ${chalk.blue('ModuleName')} ${chalk.cyan('ComponentName')}
+Please note: All names have to be ${chalk.red('UpperCamelCased')}!!
+    `);
   }
 };
